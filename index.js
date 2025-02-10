109769c4359cca30872a7b7e77faf7a9d6adce84
Pedro Henrique Simão Silva <contatophss11@gmail.com>
2025-02-10 16:51:44 -0300

    config husky

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
+// Neste exemplo se tiver variáveis ou imports sem uso o pre-commit vai acusar erro como especificado na configuração do eslint
+
 //const fs = require('fs');
 //const var_sem_uso = 'eu não tenho uso';
